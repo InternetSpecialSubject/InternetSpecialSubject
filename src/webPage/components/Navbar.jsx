7b538f4428d5981ddf622ffc2efc07ab86a026d2
posong12 <a0939222122@gmail.com>
2023-12-04 16:27:19 +0800

    test

--- src/webPage/components/Navbar.jsx
+++ src/webPage/components/Navbar.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav>
-            <div className="col-mb-3">
+            <div className="script">
                 <Link to="/" className="btn btn-primary">Home</Link>
                 <Link to="/login" className="btn btn-primary">Login</Link>
                 <Link to="/playlists" className="btn btn-primary">Playlists</Link>
