af4d04fa42c07ce8153c8d7c362b586a22094a4a
posong12 <a0939222122@gmail.com>
2023-12-05 09:46:32 +0800

    css update

--- src/webPage/components/Navbar.jsx
+++ src/webPage/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import {Link} from 'react-router-dom';
-import '../css/script.css'
+import '../css/script.css';
 export default function Navbar() {
     return (
-        <nav>
-            <div className="btn_set">
+            <div className="btn_container">
                 <Link to="/" className="btn_style">Home</Link>
                 <Link to="/login" className="btn_style">Login</Link>
                 <Link to="/playlists" className="btn_style">Playlists</Link>
             </div>
-        </nav>
     );
 }
