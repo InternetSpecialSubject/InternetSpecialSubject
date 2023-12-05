774268d680e26286f84f4c3b9a1426ea4a09c064
wangwen <c110151128@gmail.com>
2023-12-05 19:20:26 +0800

    wang

--- src/webPage/setting/SpotifyAPI.jsx
+++ src/webPage/setting/SpotifyAPI.jsx
@@ -1,5 +1,5 @@
 const SpotifyAPI = {
-    redirect_uri: 'https://4d3a-59-125-140-69.ngrok-free.app/callback',
+    redirect_uri: 'http://localhost:3000/callback',
     client_id:'ebf1bf4571374afa83e28c5c465bf0e6',
     scope:'user-read-private user-read-email playlist-read-private user-library-read',
 };
