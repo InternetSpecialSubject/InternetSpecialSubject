7939a76a8394378b03096ab36aedd855f089f32f
wangwen <c110151128@gmail.com>
2023-12-05 19:23:46 +0800

    wang

--- src/webPage/setting/SpotifyAPI.jsx
+++ src/webPage/setting/SpotifyAPI.jsx
@@ -1,7 +1,7 @@
 const SpotifyAPI = {
     redirect_uri: 'http://localhost:3000/callback',
-    client_id:'ebf1bf4571374afa83e28c5c465bf0e6',
-    scope:'user-read-private user-read-email playlist-read-private user-library-read',
+    client_id: 'ebf1bf4571374afa83e28c5c465bf0e6',
+    scope: 'user-read-private user-read-email playlist-read-private user-library-read',
 };
 
 export default SpotifyAPI;
