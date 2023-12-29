f338a4ac67da51d519fcb215c3cf0d72f162818f
posong12 <a0939222122@gmail.com>
2023-12-29 14:33:19 +0800

    css update

--- src/webPage/Home.jsx
+++ src/webPage/Home.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar";
+import './css/script.css'
 export default function Home() {
     return (
-        <div className="Home">
-            <div className="title">
-                <Navbar/>
-            </div>
+        <div className="home">
+            <div className="background_container"></div>
+                <div className="title">
+                    <Navbar/>
+                </div>
+            <p className="first_p">享受您的音樂，立即登入</p>
         </div>
     )
 }
--- src/webPage/components/Navbar.jsx
+++ src/webPage/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import {Link} from 'react-router-dom';
 import '../css/script.css';
+import logo from "../img/logo.jpg";
 export default function Navbar() {
     return (
-            <div className="btn_container">
-                <Link to="/" className="btn_style">Home</Link>
-                <Link to="/login" className="btn_style">Login</Link>
-                <Link to="/playlists" className="btn_style">Playlists</Link>
+        <nav>
+            <div className="logo-container">
+                <img alt="" className="logo_img" src={logo}></img>
+                <Link to="/login" className="btn_style">登入</Link>
+                <Link to="/playlists" className="btn_style">推薦歌曲</Link>
+                <Link to="/" className="btn_style">首頁</Link>
             </div>
+        </nav>
     );
 }
